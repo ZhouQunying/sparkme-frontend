aaafb468d5cadcc224dfb74095d9a7703c1d98fd
周群营 <richardzhou@shunshunliuxue.com>
2017-04-11 10:42:39 +0800

    Search input done.

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -16,28 +16,39 @@ import { SvgDraw } from '../svg-icon/svg-icon.component';
 })
 export class NavbarComponent implements OnInit {
 
-  @ViewChild('searchInput') el: ElementRef;
+  @ViewChild('searchInput') searchInputEl: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+    this.searchInput = <HTMLScriptElement>this.searchInputEl.nativeElement;
   }
 
-  private searchIconDraw = new SvgDraw(25, `0 0 25 25`, `M20.067 18.933l-4.157-4.157a6 6 0 1 0-.884.884l4.157 4.157a.624.624 0 1 0 .884-.884zM6.5 11c0-2.62 2.13-4.75 4.75-4.75S16 8.38 16 11s-2.13 4.75-4.75 4.75S6.5 13.62 6.5 11z`);
+  private searchIconDraw: SvgDraw = new SvgDraw(25, `0 0 25 25`, `M20.067 18.933l-4.157-4.157a6 6 0 1 0-.884.884l4.157 4.157a.624.624 0 1 0 .884-.884zM6.5 11c0-2.62 2.13-4.75 4.75-4.75S16 8.38 16 11s-2.13 4.75-4.75 4.75S6.5 13.62 6.5 11z`);
   private searchInputState: string = 'inactive';
+  private searchInput: any;
 
   private searchInputToggle() {
     if (this.searchInputState === 'active') {
-      this.searchInputState = 'inactive';
+      this.searchInputInactive();
     } else {
       this.searchInputState = 'active';
-      this.el.nativeElement.focus();
+      this.searchInput.focus();
     }
   }
 
-  private searchInputOnBlur(value) {
-    if (!value.trim()) {
-      this.searchInputState = 'inactive';
+  private searchInputOnBlur() {
+    if (!this.searchInput.value.trim()) {
+      this.searchInputInactive();
     }
+  }
+
+  private searchInputOnEnter() {
+    console.log(this.searchInput.value);
+  }
+
+  private searchInputInactive() {
+    this.searchInputState = 'inactive';
+    this.searchInput.value = '';
   }
 }
